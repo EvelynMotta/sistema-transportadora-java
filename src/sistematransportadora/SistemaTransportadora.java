3007d1a4cbd1ab7d3597d82692a9ba7210f565bf
Pedro <pedroanuda@gmail.com>
2025-04-16 23:29:11 -0300

    Inicializar projeto

---
+++ src/sistematransportadora/SistemaTransportadora.java
@@ -0,0 +1,16 @@
+package sistematransportadora;
+
+/**
+ *
+ * @author pedro
+ */
+public class SistemaTransportadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
