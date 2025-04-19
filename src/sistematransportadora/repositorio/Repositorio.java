e84b271c6fac1ab604aba7be534238f26c4c49e4
Pedro Anuda <pedroanuda@gmail.com>
2025-04-19 01:57:30 -0300

    feat: adicionar interface de Repositorio e `VeiculoRepositorio`

---
+++ src/sistematransportadora/repositorio/Repositorio.java
@@ -0,0 +1,27 @@
+package sistematransportadora.repositorio;
+
+import java.util.HashMap;
+
+/**
+ * Interface que representa um repositório.
+ * Aqui se define os métodos que cada repositório precisa ter para fazer
+ * a comunicação com o banco de dados.
+ *
+ * @param <T> O tipo com que o repositório trabalha.
+ */
+public interface Repositorio<T> {
+    // Create
+    void criarNovo(T obj);
+
+    // Read
+    T buscarPorId(int id);
+    T[] buscarTodos();
+    HashMap<String, Integer> contarPorTipo();
+    int contarTodos();
+
+    // Update
+    void atualizarUm(T obj);
+
+    // Delete
+    void apagarPorId(int id);
+}
